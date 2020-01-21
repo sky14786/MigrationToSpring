39776017bf3dab674db259d84df02aabaf2d9764
ohkiseok <border0495@naver.com>
2020-01-21 17:51:58 +0900

    crossorigin

--- src/main/java/com/themembers/member/MemberController.java
+++ src/main/java/com/themembers/member/MemberController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.themembers.common.dto.MemberDTO;
-
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api")
 public class MemberController {
