0879590ee140eeeff10533e3d505232e3798ff91
developerJY <sky14786@gmail.com>
2020-02-11 23:25:44 +0900

    Code Test

--- src/main/java/com/themembers/HomeController.java
+++ src/main/java/com/themembers/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	@RequestMapping(value = "/")
+	@RequestMapping("/")
 	public String connectMain() {
 		return "index";
 	}
